60ad7e2218c0524dabc67e24262ad315096b72d8
Alan Evans <thealanevans@gmail.com>
2019-02-17 10:52:46 -0400

    Use groovy assert

--- src/test/groovy/io/github/westonal/analysis/PackageNameTest.groovy
+++ src/test/groovy/io/github/westonal/analysis/PackageNameTest.groovy
@@ -3,20 +3,17 @@ package io.github.westonal.analysis
 import org.junit.Test
 
 import static groovy.test.GroovyAssert.shouldFail
-import static org.junit.Assert.assertEquals
-import static org.junit.Assert.assertNotEquals
 
 class PackageNameTest {
 
     @Test
     void assertEqualityContract_Equal() {
-        assertEquals(new PackageName(name: 'A'), new PackageName(name: 'A'))
-
+        assert new PackageName(name: 'A') == new PackageName(name: 'A')
     }
 
     @Test
     void assertEqualityContract_NotEqual() {
-        assertNotEquals(new PackageName(name: 'A'), new PackageName(name: 'B'))
+        assert new PackageName(name: 'A') != new PackageName(name: 'B')
     }
 
     @Test
@@ -30,6 +27,6 @@ class PackageNameTest {
 
     @Test
     void toStringFormat() {
-        assertEquals('a', new PackageName(name: 'a').toString())
+        assert new PackageName(name: 'a').toString() == 'a'
     }
 }
--- src/test/groovy/io/github/westonal/analysis/graph/EdgeTest.groovy
+++ src/test/groovy/io/github/westonal/analysis/graph/EdgeTest.groovy
@@ -1,46 +1,46 @@
 package io.github.westonal.analysis.graph
 
-import groovy.test.GroovyAssert
-import org.junit.Assert
 import org.junit.Test
+
+import static groovy.test.GroovyAssert.shouldFail
 
 class EdgeTest {
 
     @Test
     void create() {
         def edge = new Edge<String>("From", "To")
-        Assert.assertSame(edge.from, "From")
-        Assert.assertSame(edge.to, "To")
+        assert edge.from == "From"
+        assert edge.to == "To"
     }
 
     @Test
     void assertEqualityContract_Equal() {
-        Assert.assertEquals(new Edge<String>("From", "To"), new Edge<String>("From", "To"))
+        assert new Edge<String>("From", "To") == new Edge<String>("From", "To")
     }
 
     @Test
     void assertEqualityContract_NotEqual() {
-        Assert.assertNotEquals(new Edge<String>("From", "To"), new Edge<String>("To", "From"))
+        assert new Edge<String>("From", "To") != new Edge<String>("To", "From")
     }
 
     @Test
     void immutable() {
         def edge = new Edge<Integer>(1, 2)
-        GroovyAssert.shouldFail {
+        shouldFail {
             //noinspection GroovyAccessibility,GrFinalVariableAccess
             edge.from = 3
         }
-        GroovyAssert.shouldFail {
+        shouldFail {
             //noinspection GroovyAccessibility,GrFinalVariableAccess
             edge.to = 3
         }
-        Assert.assertSame(edge.from, 1)
-        Assert.assertSame(edge.to, 2)
+        assert edge.from == 1
+        assert edge.to == 2
     }
 
     @Test
     void toStringFormat() {
         def edge = new Edge<String>("A", "B")
-        Assert.assertEquals("A -> B", edge.toString())
+        assert edge.toString() == "A -> B"
     }
 }
